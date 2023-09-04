67d384bc738b7ba44026cf8d6acf07f8af9c9776
vonesh <79078924+vonesh@users.noreply.github.com>
2023-09-04 11:29:22 -0400

    Add files via upload

---
+++ header.php
@@ -0,0 +1,27 @@
+<?php
+include('conn.php');
+
+?>
+<style type="text/css">    .top {
+        position:absolute;
+        left:0; right:0;
+        height: 70px;
+    }
+    .left {
+        position:absolute;
+        left:0; top:70px; bottom: 0;
+        width: 178px;
+    }
+    .main {
+        position: absolute;
+        left:178px; top:70px; right:0; bottom:0; width: 100%;
+    }
+
+.top { background: lightblue; }
+.left { background: lightgray; }
+.main { background: lightyellow; }
+
+</style>
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous"> 
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
