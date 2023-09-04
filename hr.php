bd9485dbf65b7a6cac96f3d0a81347cbd2135006
vonesh <79078924+vonesh@users.noreply.github.com>
2023-09-04 11:28:40 -0400

    Add files via upload

---
+++ hr.php
@@ -0,0 +1,14 @@
+
+<nav class="navbar navbar-dark" style="background-color:lightblue;">
+
+    <div class="container">
+        
+            
+            <div class="col"></div><div class="col"><h1 style="text-align:center;"><legend>Cricket Tourament</legend></h1></div><div class="col"></div>
+       
+    </div>
+</nav>
+
+<div class="top">
+
+</div>
